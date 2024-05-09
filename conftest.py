be7e3a45ba1fbeee09961934bd49fb291edfb965
almatyAlma <168709548+almatyAlma@users.noreply.github.com>
2024-05-09 14:01:24 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,11 +1,11 @@
 import pytest
 from selene import browser
 
-
-@pytest.fixture(autouse=True )
-def browser_management():
+@pytest.fixture
+def browser_size():
     browser.config.base_url = 'https://google.com'
     browser.config.window_height = 800
     browser.config.window_width = 1200
     yield
     browser.quit()
+
